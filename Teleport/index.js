4cf8fe327e2629ff73141e41526bf736dd0ce689
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-09-01 20:17:27 +0300

    js modify data

--- Teleport/index.js
+++ Teleport/index.js
@@ -1,21 +1,29 @@
-const randomCity = "(https://api.teleport.org/api/";
+const randomCity = "https://api.teleport.org/api/urban_areas/teleport%3A9q8yy/scores/";
 const cities = document.getElementById("btn").addEventListener("click",getCity);
+const cityIwant = document.getElementById("port-1");
 
 
 function getCity(){
     return document.getElementById("city").value;
 }
-fetch("(https://api.teleport.org/api/")
-.then((res) =>{res.json()})
-.then((completeData) => {completeData._embedded["city:search-results"]
-    let inputCity= "";
-    let towns = completeData?.map((values)=>{
-        inputCity += `${values}`
-    });
-    
-    if(cities === values){
-        return towns;
-    }
-})
-.catch((error) =>{console.log(error)})
 
+fetch("https://api.teleport.org/api/urban_areas/teleport%3A9q8yy/scores/")
+.then(response =>response.json())
+.then(cityDom)
+.catch((error) =>(error))
+
+function cityDom(data){
+    cityIwant.textContent = data.alternate_names[1].name
+}
+
+
+
+
+    // let inputCity= "";
+    // let towns = completeData?.map((values)=>{
+    //     inputCity += `${values}`
+    // });
+    
+    // if(cities === city){
+    //     return towns;
+    // }
